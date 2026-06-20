8432a03429ab16bb124747b7b9aad2cfc05c33d9
jerowm098 <jjjjema098@gmail.com>
2026-06-20 20:15:59 +0800

    awawdadawd

--- pages/nurse/nurse_logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// capstonemain/pages/nurse/nurse_logout.php
-session_start();
-session_destroy();
-header('Location: nurse_login.php');
-exit();
-?>
--- pages/student/student_logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// capstonemain/pages/student/student_logout.php
-session_start();
-session_destroy();
-header('Location: student_login.php');
-exit();
-?> 
